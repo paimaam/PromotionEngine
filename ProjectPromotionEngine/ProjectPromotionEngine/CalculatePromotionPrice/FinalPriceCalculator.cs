c134fca8adc08f407bad9a1cf27300a80fe99124
Dileep Dayanand Pai <dilludp123@gmail.com>
2020-06-25 23:05:34 +0530

    Added case to calculate for A

--- ProjectPromotionEngine/ProjectPromotionEngine/CalculatePromotionPrice/FinalPriceCalculator.cs
+++ ProjectPromotionEngine/ProjectPromotionEngine/CalculatePromotionPrice/FinalPriceCalculator.cs
@@ -14,7 +14,48 @@ namespace ProjectPromotionEngine.CalculatePromotionPrice
 
         public int  CalculateFinalPrice(GetQuantityDetails quantityDetails)
         {
-            return 100;
+            var (asku, bsku, csku, dsku) = ConvertToInteger(quantityDetails);
+
+            asku = TotalPriceOfA(asku);
+            return asku;
+
+        }
+
+        private (int, int, int, int ) ConvertToInteger(GetQuantityDetails quantityDetails)
+        {
+            int a, b, c, d;
+            try
+            {
+                 a = Int32.Parse(quantityDetails.AQty);
+                 b = Int32.Parse(quantityDetails.BQty);
+                 c = Int32.Parse(quantityDetails.CQty);
+                 d = Int32.Parse(quantityDetails.DQty);
+
+
+            }
+            catch (FormatException e)
+            {
+                return (0, 0, 0, 0);
+            }
+
+            return (a, b, c, d);
+        }
+
+        private int TotalPriceOfA(int quantityOfA)
+        {
+            var (quotient, remainder) = DivideAndGetQuotient(quantityOfA, 3);
+            var price = quotient * 130;
+            var subPrice = remainder * 50;
+
+            return price + subPrice;
+        }
+
+        private (int, int) DivideAndGetQuotient(int divident, int divisor)
+        {
+            var remainder = divident % divisor;
+            var quotient = divident / divisor;
+
+            return (quotient, remainder);
         }
     }
 }
