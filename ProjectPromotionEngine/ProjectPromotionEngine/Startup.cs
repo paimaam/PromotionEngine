7bc2fad9e6462684294d9b23347a5b7a239f6578
Dileep Dayanand Pai <dilludp123@gmail.com>
2020-06-26 08:14:05 +0530

    Added newtonsoft json to dll to deserialize

--- ProjectPromotionEngine/ProjectPromotionEngine/Startup.cs
+++ ProjectPromotionEngine/ProjectPromotionEngine/Startup.cs
@@ -23,7 +23,7 @@ namespace ProjectPromotionEngine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSingleton<IFinalPriceCalculatorService, FinalPriceCalculator>();
         }
 
